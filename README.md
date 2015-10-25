f3fe27f7f2334c146df5073e5a64eefeaecf87ad
solens <s.solen@hotmail.com>
2015-10-25 07:28:20 +0100

    could be done, should run the code once more

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # getdata-033
 Course Project
+##DATA
+original data from:
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
+
+Data link:
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+
+## Run the analysis
+
+
+run_analysis.R: requires the data (UCI HAR Dataset file) in the working directory
+
+->creates two tables:
+1.tidy table of all measurements of mean and standard deviation per subject per activity
+
+2.table of the average for each feature involved in the tidy data per subject per activity
---
+++ run_analysis.R
@@ -0,0 +1,49 @@
+#load necessary packages
+library(dplyr)
+
+#Get all relevant data from directory
+dir<-getwd()
+x_test<-read.table(paste(dir,"/UCI HAR Dataset/test/X_test.txt",sep=""))
+y_test<-read.table(paste(dir,"/UCI HAR Dataset/test/Y_test.txt",sep=""))
+subject_test<-read.table(paste(dir,"/UCI HAR Dataset/test/subject_test.txt",sep=""))
+
+x_train<-read.table(paste(dir,"/UCI HAR Dataset/train/X_train.txt",sep=""))
+y_train<-read.table(paste(dir,"/UCI HAR Dataset/train/Y_train.txt",sep=""))
+subject_train<-read.table(paste(dir,"/UCI HAR Dataset/train/subject_train.txt",sep=""))
+
+features<-read.table(paste(dir,"/UCI HAR Dataset/features.txt",sep=""))
+activity_labels<-read.table(paste(dir,"/UCI HAR Dataset/activity_labels.txt",sep=""))
+
+#Get relevant data: all features containing "mean()" or "std()"
+#assuming they are mutually exclusive
+usable_index<-grepl("mean()",features[,2],fixed=TRUE)
+usable_index2<-grepl("std()",features[,2],fixed=TRUE)
+use_index<-as.logical(usable_index+usable_index2)
+
+values<-rbind(x_test[,use_index],x_train[,use_index])
+activity<-rbind(y_test,y_train)
+subject<-rbind(subject_test,subject_train)
+feat<-as.character(features[use_index,2])
+
+full_act_label<-mapvalues(simplify2array(activity),from=activity_labels[,1],to=activity_labels[,2])
+
+tidy_data<-cbind(subject,full_act_label,values)
+
+names(tidy_data)<-c("subject","activity",feat)
+
+##melt features data into long dataframe, then separate the feature components to get cols:
+##subject / activity / motion / statElement / direction / value
+## long_data is the tidy data required for further analysis
+melt_data<-melt(tidy_data,names(tidy_data[1:2]),names(tidy_data[-c(1,2)])) 
+write.table(melt_data,file="melt_data.txt")
+averages<-with(melt_data,tapply(value,list(subject,activity,variable),mean))
+melt_average<-melt(averages)
+write.table(melt_average,file="melt_average.txt")
+long_data<-separate(melt_data,variable,c("motion","statElement","direction"))
+
+
+
+
+
+
+
